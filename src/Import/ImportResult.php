8f51d23c33580d999c9a9586d09f57eb224020b6
soleros <soleromel@gmail.com>
2026-07-11 10:56:58 +0200

    feat(import): add import result DTOs

---
+++ src/Import/ImportResult.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Import;
+
+final class ImportResult
+{
+    /**
+     * @param list<FileImportResult> $files
+     */
+    public function __construct(
+        public readonly array $files,
+    ) {
+    }
+
+    public function hasFailures(): bool
+    {
+        foreach ($this->files as $file) {
+            if ($file->isFailure()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function totalImported(): int
+    {
+        $total = 0;
+        foreach ($this->files as $file) {
+            $total += $file->importedCount;
+        }
+
+        return $total;
+    }
+}
